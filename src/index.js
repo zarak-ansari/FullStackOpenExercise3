c7a020567a9047bfd676395a21dd54813c164959
Zarak Ansari <zarak.ansari@yahoo.com>
2023-05-23 09:53:44 +0500

    completed till 3-18

--- src/index.js
+++ src/index.js
@@ -14,10 +14,17 @@ const app = express()
 app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
-// app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'))
 
 const PORT = process.env.PORT
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if(error.name === 'CastError'){
+        response.status(400).send({error:"malformatted id"})
+    }
+    next(error)
+}
 
 app.get("/info", (request, response) => {
     const currDate = new Date()
@@ -26,17 +33,26 @@ app.get("/info", (request, response) => {
     })
 })
 
-app.get("/persons", (request, response) => {
-    Person.find({}).then(persons => response.json(persons))
+app.get("/persons", (request, response, next) => {
+    Person.find({})
+        .then(persons => response.json(persons))
+        .catch(error => next(error))
 })
 
-app.get("/persons/:id", (request, response) => {
+app.get("/persons/:id", (request, response, next) => {
     Person
         .findById(request.params.id)
-        .then(person => response.json(person))
+        .then(person => {
+            if(person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.post("/persons", (request, response) => {
+app.post("/persons", (request, response, next) => {
     const personSent = request.body
 
     if (!personSent.name || !personSent.number) {
@@ -44,15 +60,32 @@ app.post("/persons", (request, response) => {
     }
 
     const person = new Person(request.body)
-    person.save().then(personOnServer => response.json(personOnServer))
+    person.save()
+        .then(personOnServer => response.json(personOnServer))
+        .catch(error => next(error))
 })
 
-app.delete("/persons/:id", (request, response) => {
+app.delete("/persons/:id", (request, response, next) => {
     Person
         .findByIdAndRemove(request.params.id)
         .then(response.status(200).end())
-        .catch(err => console.log(err))
+        .catch(error => next(error))
 })
+
+app.put("/persons/:id", (request, response, next) => {
+    const newPerson = {
+        name: request.body.name,
+        number: request.body.number
+    }
+    Person
+        .findByIdAndUpdate(request.params.id, newPerson, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
+})
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
